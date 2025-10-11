e632b9a82d7eb0fef5e433bdb0dff3ec1a9bb150
we5046 <76059661+we5046@users.noreply.github.com>
2025-10-11 21:36:34 +0900

    Add files via upload

---
+++ Select 모델/CRingBuffer.h
@@ -0,0 +1,28 @@
+
+#pragma once
+class CRingBuffer
+{
+private:
+	char* buffer;
+	int bufferSize;
+	int useSize;
+	int front;
+	int rear;
+public:
+	CRingBuffer();
+	CRingBuffer(int iBufferSize);
+	~CRingBuffer();
+	int GetBufferSize();
+	int GetUseSize();
+	int GetFreeSize();
+	int Enqueue(const char* chpData, int iSize);
+	int Dequeue(char* chpData, int iSize);
+	int Peek(char* chpDest, int iSize);
+	void ClearBuffer(void);
+	int DirectEnqueueSize();
+	int DirectDequeueSize();
+	int MoveRear(int iSize);
+	int MoveFront(int iSize);
+	char* GetFrontBufferPtr(void);
+	char* GetRearBufferPtr(void);
+};
